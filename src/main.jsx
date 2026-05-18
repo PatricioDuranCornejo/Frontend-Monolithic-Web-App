5d8bc8f1e881b1b4544fa313ae803aaf28fad53e
PatricioDuranCornejo <patricio.duran.c@usach.cl>
2026-05-18 04:33:24 -0400

    Cloud server implementation

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,30 @@ import './index.css'
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./services/keycloak";
 
+const eventLogger = (event, error) => {
+  console.log('Keycloak Event:', event);
+  if (e |rror) {
+    console.error('Keycloak Error:', error);
+  }
+};
+
+const keycloakInitOptions = {
+  onLoad: 'login-required',
+  checkLoginIframe: false,
+  pkceMethod: 'S256',
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReactKeycloakProvider authClient={keycloak}>
+  <ReactKeycloakProvider 
+    authClient={keycloak}
+    initOptions={keycloakInitOptions}
+    onEvent={eventLogger}
+    LoadingComponent={
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+        <h2>Cargando...</h2>
+      </div>
+    }
+  >
     <App />
   </ReactKeycloakProvider>
 )
