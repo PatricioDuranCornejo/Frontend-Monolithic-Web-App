614b4f45b3ffd029c3651b2c439fcbc713f643ee
PatricioDuranCornejo <patricio.duran.c@usach.cl>
2026-07-13 00:18:03 -0400

    .yml for Github Pipeline

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:9090",
+  url: "http://24.199.87.22:9090",
   realm: "Monolithic-Realm",
   clientId: "Monolithic-Frontend",
 });
